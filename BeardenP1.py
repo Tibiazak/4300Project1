c59ea2fa8f8a4d91ca765158d39a9b411a6dc7e9
Josh <josh.e.bearden@gmail.com>
2018-02-18 13:41:55 -0600

    Added a function search Added failure states to invalid

--- BeardenP1.py
+++ BeardenP1.py
@@ -17,36 +17,75 @@ def performop(state, op):
     return newstate
 
 
-def isvalid(state):
+def isvalid(state, startstate):
+    otherside = startstate.copy()
+    for i in range(0, 3):
+        otherside[i] = startstate[i] - state[i]
     if state[1] > state[0]:
+        return False
+    elif state[0] > 3 or state[1] > 3 or state[2] > 1:
+        return False
+    elif otherside[1] > otherside[0]:
+        return False
+    elif otherside[0] > 3 or otherside[1] > 3 or otherside[2] > 1:
         return False
     else:
         return True
 
 
-def gensuccessors(ops, state):
+def gensuccessors(ops, state, startstate):
     successors = []
     validops = []
     for op in ops:
         new = performop(state, op)
-        if isvalid(new):
+        if isvalid(new, startstate):
             successors.append(new)
             validops.append(op)
     return successors, validops
 
 
+def search(node, depth, goal, ops, startstate):
+    if node == goal:
+        return []
+    elif depth > 0:
+        transitions, valops = gensuccessors(ops, node, startstate)
+        # print("Transitions: ", str(transitions), "for node: ", str(node))
+        for state in transitions:
+            result = search(state, depth-1, goal, ops, startstate)
+            if result is not None:
+                # print("Result is :", str(result), "Operation is: ", str(valops[transitions.index(state)]))
+                result.append(valops[transitions.index(state)])
+                return result
+        return None
+    else:
+        return None
+
+
+# Initiate variables - startstate is 3 missionaries, 3 cannibals, and a boat
+# goalstate is 0 missionaries, 0 cannibals, no boat
+
 startstate = [3, 3, 1]
 goalstate = [0, 0, 0]
 operations = [[0, 1, 1], [1, 0, 1], [1, 1, 1], [2, 0, 1], [0, 2, 1]]
+maxdepth = 11
+found = False
+# frontier, validops = gensuccessors(operations, startstate)
+# print(frontier)
+# print(validops)
+# for state in frontier:
+#     if state == goalstate:
+#         print("Found solution with operation: ")
+#         print(validops[frontier.index(state)])
+#         break
+#     else:
+#         print("No solution found.")
 
-frontier, validops = gensuccessors(operations, startstate)
-print(frontier)
-print(validops)
-for state in frontier:
-    if state == goalstate:
-        print("Found solution with operation: ")
-        print(validops[frontier.index(state)])
+for i in range(0, maxdepth):
+    result = search(startstate, maxdepth, goalstate, operations, startstate)
+    if result is not None:
+        result.reverse()
+        print(result)
+        found = True
         break
-    else:
-        print("No solution found.")
-
+if not found:
+    print("Goal state not found at depth ", str(maxdepth))
